1a481218f02ef0df4577524c07df4ccbc41eeb24
mmmpa <mmmpa.mmmpa@gmail.com>
2016-07-27 21:14:40 +0900

    updated

--- app/controllers/all_controller.rb
+++ app/controllers/all_controller.rb
@@ -1,65 +1,61 @@
-require 'uri'
-
 class AllController < ApplicationController
   cattr_accessor :stored_cookies
+  self.stored_cookies = {}
 
   def all
     headers['Access-Control-Allow-Credentials'] = 'true'
     headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-    headers['Access-Control-Allow-Origin'] = 'http://localhost:8080'
+    headers['Access-Control-Allow-Origin'] = ENV['ALLOW']
     headers['Access-Control-Request-Method'] = '*'
     headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
 
-    p kicked
     case kicked
       when RestClient::Response
         store_cookie(kicked.cookies)
 
         begin
-          render json: JSON.parse(kicked.body), status: kicked.code and return
+          render json: JSON.parse(kicked.body), status: kicked.code
         rescue
-          render plain: kicked.body, status: kicked.code and return
+          render plain: kicked.body, status: kicked.code
         end
       when RestClient::InternalServerError
-        render head: :internal_server_error and return
+        head :internal_server_error
       when RestClient::BadRequest
-        render head: :bad_request and return
+        head :bad_request
       else
-        render plain: kicked and return
+        render plain: kicked
     end
   end
 
   private
 
+  def request_id
+    # because session.id == nil in a request with OPTIONS
+    1
+  end
+
   def store_cookie(val)
-    stored_cookies[request_id] = val
+    self.class.stored_cookies[request_id] = val
   end
 
   def stored_cookie
-    stored_cookies[request_id]
-  end
-
-  def stored_cookies
-    self.class.stored_cookies ||= {}
-  end
-
-  def request_id
-    session[:request_id] ||= SecureRandom.uuid
+    self.class.stored_cookies[request_id] || {}
   end
 
   def kicked
-    p stored_cookie.to_h
-    @kicked = begin
-      if method == 'get' || method == 'options'
-        uri = URI(target)
-        uri.query = posted.to_param
-        RestClient.send(method, uri.to_s, {cookies: stored_cookie})
-      else
+    @kicked ||= begin
+      if posting?
         RestClient.send(method, target, posted, {cookies: stored_cookie})
+      else
+        RestClient.send(method, queried, {cookies: stored_cookie})
       end
     rescue => e
-      p e
+      e
     end
+  end
+
+  def posting?
+    method != 'get' && method != 'options'
   end
 
   def path
@@ -79,6 +75,12 @@ class AllController < ApplicationController
   end
 
   def target
-    p Pathname(ENV['TARGET']).join(path).to_s
+    Pathname(ENV['TARGET']).join(path).to_s
+  end
+
+  def queried
+    URI(target).tap { |uri|
+      uri.query = posted.to_param
+    }.to_s
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,3 @@
 Rails.application.routes.draw do
-  get '*path' => 'all#all'
-  post '*path' => 'all#all'
-  put '*path' => 'all#all'
-  patch '*path' => 'all#all'
-  delete '*path' => 'all#all'
-  match '*path' => 'all#all', via: :options
-
-  get '/' => 'all#all'
-
-  get '/test' => 'all#test'
+  match '*path' => 'all#all', via: :all
 end
